5beb2bda091f4cb1d539618190ea254b76d6db15
Cornelis Kettenis <cornelis@sig.com.my>
2026-08-28 12:33:02 +0800

    feat: verify 0.1.1 release binaries

--- checksum-Elixir.ElixirFontDiscovery.Native.exs
+++ checksum-Elixir.ElixirFontDiscovery.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "elixir_font_discovery-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:8eceb7bb3a604ab033a869ebc9d48d1c40a1308ad9c1ae3d2b84a156f5976aaa",
-  "elixir_font_discovery-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:79e91f71d411b64bcb755918e90f530cfec6c4cefba6a827df0d4053d6cd3dcb",
-  "libelixir_font_discovery-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:43cd74509c232a05b1d57bb8ba1906fd4a77120da82a8406c4bdf91872070495",
-  "libelixir_font_discovery-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f9e5ebd26f2f48a932c33f9b8a546ac1f9b3f73576a76e4808c09466a57f725b",
-  "libelixir_font_discovery-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:22f29007e9b8a0f1fce654fcffd59ec67fd3821b1a8e345987121f189d3d99b1",
-  "libelixir_font_discovery-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c359d39f89a1954e564a79feefe78eaa9fb4f3431959aa516da95f57163228c4",
+  "elixir_font_discovery-v0.1.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:3ecee968dc46809c00087c6fe23f92c5dcaf557f5ddab742317bbbe2a8bb81b3",
+  "elixir_font_discovery-v0.1.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:065ecf02a415969bd33f389e66f046bdafb6c0e128a23837246c9c9cd54559c7",
+  "libelixir_font_discovery-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:9c79c5417e51587afb5d12567a44e8543a29467291029cd0926296d0ee10db5f",
+  "libelixir_font_discovery-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:42c90e32b407fc82106afc38f0f90581bbe8fcf6dced910c850b62a9ea6f2d88",
+  "libelixir_font_discovery-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:13e759ca7ca77dc40b87adeabc9d601db7a63b7d027f926302cbcaccc1640dc4",
+  "libelixir_font_discovery-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:777ce56faeaff2b13c77e3666b1592d2b61a58eed71129f2061985d77a4a5d89",
 }
