46358725c74a48d61f2e3e3ee19d08bcd9a19fae
Cees Kettenis <cees9000@gmail.com>
2026-08-29 21:43:25 +0800

    feat: verify 0.2.0 release binaries

--- checksum-Elixir.ElixirFontDiscovery.Native.exs
+++ checksum-Elixir.ElixirFontDiscovery.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "elixir_font_discovery-v0.1.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:3ecee968dc46809c00087c6fe23f92c5dcaf557f5ddab742317bbbe2a8bb81b3",
-  "elixir_font_discovery-v0.1.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:065ecf02a415969bd33f389e66f046bdafb6c0e128a23837246c9c9cd54559c7",
-  "libelixir_font_discovery-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:9c79c5417e51587afb5d12567a44e8543a29467291029cd0926296d0ee10db5f",
-  "libelixir_font_discovery-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:42c90e32b407fc82106afc38f0f90581bbe8fcf6dced910c850b62a9ea6f2d88",
-  "libelixir_font_discovery-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:13e759ca7ca77dc40b87adeabc9d601db7a63b7d027f926302cbcaccc1640dc4",
-  "libelixir_font_discovery-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:777ce56faeaff2b13c77e3666b1592d2b61a58eed71129f2061985d77a4a5d89",
+  "elixir_font_discovery-v0.2.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:8dc635cb8e0d5805519800650753a1523abbdccc0f1fa29fd27854fce8f9cf2d",
+  "elixir_font_discovery-v0.2.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:a85827008dd7c24958e2f4f062384011ac9ecf5ab17ca421884fbad4638f21fd",
+  "libelixir_font_discovery-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:48fbe21fa3604687596a8a5c286c7dd74b5458a4cfda108d981f0c0f08bd7046",
+  "libelixir_font_discovery-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:73adaadd32b32b48d9a54017666573ae8bae798acd9f75288eb4a2f2bf5e4a26",
+  "libelixir_font_discovery-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ced79663262c233ca3c5dc85439daad3081e1e68399d480783fc7d27bf7880e5",
+  "libelixir_font_discovery-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:44e32587700c3ee879c4f1f0cf4faf4914c2042daf4e0e28d6b3adb700ab097e",
 }
